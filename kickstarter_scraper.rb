ebdd4fb85262218033bf22d02f6c43a665bc10c1
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-20 13:34:14 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,5 @@ end
 create_project_hash
 #projects: kickstarter.css("li.project.grid_4").first
 #title: project.css("h2.bbcard_name strong a").text
+#image: project.css("div.project.thumbnail a img").attribute("src").value
 binding.pry
